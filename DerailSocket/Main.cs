c07e95d60d228819c8c98418d3bf771b5fdd940a
chase <chase@DESKTOP-QT0BPI1>
2023-07-01 16:02:58 +0200

    Add project files.

---
+++ DerailSocket/Main.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+
+namespace DerailSocket
+{
+    static class Main
+    {
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = OnToggle;
+            return true;
+        }
+
+        // Called when the mod is turned to on/off.
+        // With this function you control an operation of the mod and inform users whether it is enabled or not.
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value /* active or inactive */)
+        {
+            if (value)
+            {
+                Run(); // Perform all necessary steps to start mod.
+            }
+            else
+            {
+                Stop(); // Perform all necessary steps to stop mod.
+            }
+
+            enabled = value;
+            return true; // If true, the mod will switch the state. If not, the state will not change.
+        }
+
+        static void Run() { Debug.Log("Run"); }
+        static void Stop() { Debug.Log("Stop"); }
+    }
+}
